44de203dba362e774998d1ca40a78d4a83616db6
Lucasdevmake <lucasanc14@gmail.com>
2023-02-15 09:25:27 -0300

    lucas: atualização de modelagem final

---
+++ Scripts/ScriptInsert.sql
@@ -0,0 +1,88 @@
+INSERT INTO Estado (nome, uf) VALUES
+    ('Acre', 'AC'),
+    ('Alagoas', 'AL'),
+    ('Amapá', 'AP'),
+    ('Amazonas', 'AM'),
+    ('Bahia', 'BA'),
+    ('Ceará', 'CE'),
+    ('Distrito Federal', 'DF'),
+    ('Espírito Santo', 'ES'),
+    ('Goiás', 'GO'),
+    ('Maranhão', 'MA'),
+    ('Mato Grosso', 'MT'),
+    ('Mato Grosso do Sul', 'MS'),
+    ('Minas Gerais', 'MG'),
+    ('Pará', 'PA'),
+    ('Paraíba', 'PB'),
+    ('Paraná', 'PR'),
+    ('Pernambuco', 'PE'),
+    ('Piauí', 'PI'),
+    ('Rio de Janeiro', 'RJ'),
+    ('Rio Grande do Norte', 'RN'),
+    ('Rio Grande do Sul', 'RS'),
+    ('Rondônia', 'RO'),
+    ('Roraima', 'RR'),
+    ('Santa Catarina', 'SC'),
+    ('São Paulo', 'SP'),
+    ('Sergipe', 'SE'),
+    ('Tocantins', 'TO');
+
+INSERT INTO TipoEstabelecimento (descricao) VALUES
+    ('Supermercado'),
+    ('Farmácia'),
+    ('Loja de Roupas'),
+    ('Padaria'),
+    ('Livraria'),
+    ('Academia'),
+    ('Restaurante'),
+    ('Cinema'),
+    ('Banco'),
+    ('Casa de Shows'),
+    ('Bar'),
+    ('Museu'),
+    ('Salão de Beleza'),
+    ('Posto de Gasolina'),
+    ('Pet Shop'),
+    ('Hospital'),
+    ('Pizzaria'),
+    ('Cafeteria'),
+    ('Teatro'),
+    ('Parque');
+
+INSERT INTO TipoPagamentoAssinatura (descricao) VALUES
+    ('Cartão de Crédito'),
+    ('Cartão de Débito'),
+    ('Boleto Bancário'),
+    ('Cheque'),
+    ('Pix');
+
+INSERT INTO TipoPagamentoIngresso (descricao) VALUES
+    ('Cartão de Crédito'),
+    ('Cartão de Débito'),
+    ('Boleto Bancário'),
+    ('Cheque'),
+    ('Dinheiro')
+    ('Pix');
+
+INSERT INTO TipoEvento (descricao) VALUES 
+    ('Treinamento'),
+    ('Palestra'),
+    ('Exposição'),
+    ('Concerto'),
+    ('Feira'),
+    ('Congresso'),
+    ('Encontro de Negócios'),
+    ('Festival'),
+    ('Seminário'),
+    ('Convenção'),
+    ('Fórum'),
+    ('Lançamento de Produto'),
+    ('Leilão'),
+    ('Workshop'),
+    ('Apresentação Teatral'),
+    ('Mostra de Cinema'),
+    ('Feijoada');
+
+
+
+
